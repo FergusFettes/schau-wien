0d9d565fbde929542176884bff7b738804c06de4
fergus <fergusfettes@gmail.com>
2021-01-11 23:35:35 +0100

    initial test build

--- cronjobs/get_blogs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo this will be a cronjob for getting the contents of the blogs
--- cronjobs/get_css.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo this will be a script for scraping the css from the jsfiddle. will probably write it in python actually
